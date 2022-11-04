2a93446fc5733c7bd1c1e59483c77baa32ae4a09
Steven <sjcastro95@icloud.com>
2022-11-03 23:25:26 -0400

    Logical operators

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Operators
+// Mod
+// Modulus (remainder)
+
+//Assignment Operators
+/**
+ *  =  x = y
+ * +=  x = x + y (x += y)
+ * -=  x = x - y
+ * *=  x = x * y
+ * /=  x = x / y
+ * %=  x = x % y
+ * **= x = x ** y
+ */
+
+/**
+ * Comparison Operators
+ *  ==
+ *  ===
+ *
+ */
+
+console.log(1 === "1");
